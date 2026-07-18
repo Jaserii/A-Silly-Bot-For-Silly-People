2d409aa05a099ed121e78e779db2658c07ac457c
Ralph Terte <109493278+Jaserii@users.noreply.github.com>
2026-07-18 12:40:55 -0400

    Create trivia feature

---
+++ src/main/java/com/jaserii/sillybot/TriviaService.java
@@ -0,0 +1,90 @@
+package com.jaserii.sillybot;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class TriviaService {
+    final String IO_ERR = "IO_ERR";
+    final String INTERRUPT_ERR = "INTERRUPT_ERR";
+
+    public String getTrivia() {
+        String apiUrl = System.getenv("TRIVIA_API_URL");
+
+        if (apiUrl == null) {
+            return "Missing API URL for Open Trivia DB";
+        }
+
+        String response = getResult(apiUrl);
+        switch (response) {
+            case IO_ERR:
+                return "Erhm... IO Error!";
+            case INTERRUPT_ERR:
+                return "Erhm... I got interrupted!";
+            default:
+                try {
+                    return parseResult(response);
+                } catch (JsonProcessingException e) {
+                    return "Something's wrong with the JSON";
+                }
+        }
+    }
+
+
+    /// Gather a random fact from Open Trivia DB
+    /// @param api The API url
+    /// @return Returns either the trivia Q&A or an error code
+    private String getResult(String api) {
+        try (HttpClient client = HttpClient.newHttpClient()) {
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(api))
+                    .GET()
+                    .build();
+
+            try {
+                HttpResponse<String> response = client.send(
+                        request,
+                        HttpResponse.BodyHandlers.ofString()
+                );
+
+                return response.body();
+
+            } catch (IOException e) {
+                return IO_ERR;
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return INTERRUPT_ERR;
+            }
+        }
+    }
+
+
+    /// Parse the question and correct answer from the response body
+    /// @param responseBody Contains the response body
+    /// @return Return parsed result
+    private String parseResult(String responseBody) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode root = objectMapper.readTree(responseBody);
+
+        JsonNode firstResult = root
+                .get("results")
+                .get(0);
+
+        String question = firstResult
+                .get("question")
+                .asText();
+
+        String correctAnswer = firstResult
+                .get("correct_answer")
+                .asText();
+
+        return question + " \n||" + correctAnswer + "||";
+    }
+}
